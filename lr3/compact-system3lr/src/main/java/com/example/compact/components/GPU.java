a2c797e790f3a8913a99f47edd637bfbea545e6f
Aleks-Buruchenko <79582322+Aleks-Buruchenko@users.noreply.github.com>
2025-06-02 20:41:24 +0300

    Delete lr3/compact-system3lr directory

--- lr3/compact-system3lr/src/main/java/com/example/compact/components/GPU.java
+++
@@ -1,9 +0,0 @@
-package com.example.compact.components;
-
-import java.util.List;
-
-public class GPU extends SystemComponent {
-    public GPU(String id, String name, String manufacturer, List<String> compatibleIds) {
-        super(id, "GPU", name, manufacturer, compatibleIds);
-    }
-}
