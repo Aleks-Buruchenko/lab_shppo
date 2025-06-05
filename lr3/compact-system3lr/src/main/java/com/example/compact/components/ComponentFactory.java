308fa0e2e3334fbc9cfc29bcca624cdd02b4a756
Aleks-Buruchenko <79582322+Aleks-Buruchenko@users.noreply.github.com>
2025-06-05 13:15:56 +0300

    Delete lr3/compact-system3lr directory

--- lr3/compact-system3lr/src/main/java/com/example/compact/components/ComponentFactory.java
+++
@@ -1,7 +0,0 @@
-package com.example.compact.components;
-
-import java.util.List;
-
-public interface ComponentFactory {
-    SystemComponent createComponent(String id, String name, String manufacturer, List<String> compatibleIds);
-}
